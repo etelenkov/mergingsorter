7727f04f4c3e86268a71ebf46ce1ada31d140519
etelenkov <etelenkov@alliedtesting.com>
2016-05-09 19:17:50 +0300

    Added test usage of Scanner for new task in App.java (files combiner task) Added test files (A.txt, B.txt) for new task

--- src/main/java/com/alliedtesting/etelenkov/myapp/App.java
+++ src/main/java/com/alliedtesting/etelenkov/myapp/App.java
@@ -1,15 +1,34 @@
 package com.alliedtesting.etelenkov.myapp;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class App {
 
     public static void main(String[] args) {
-        // Logging system simple test example
-        Logger logger = LoggerFactory.getLogger(App.class);
-        logger.info("Hello World");
-        System.out.println("Hello World!");
+//        // Logging system simple test example
+//        Logger logger = LoggerFactory.getLogger(App.class);
+//        logger.info("Hello World");
+//        System.out.println("Hello World!");
+
+        try {
+            Scanner fileAScanner = new Scanner(new File("A.txt")).useDelimiter(Pattern.compile("[\\r\\n]+"));
+            Scanner fileBScanner = new Scanner(new File("B.txt")).useDelimiter(Pattern.compile("[\\r\\n]+"));
+
+            Iterator<String> it = Iterators.sortingCombiner(
+                    Arrays.asList(fileAScanner, fileBScanner),
+                    (a, b) -> Integer.parseInt(a) - Integer.parseInt(b));
+
+            System.out.println(Iterators.toList(it));
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
+
 
 }
